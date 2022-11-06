9554e91a15e8e7d7848dd3156a5ea852ca5702e1
keeforever <kaliyamoorthy.2k@gmail.com>
2022-11-06 15:57:19 +0000

    chore: maintenance

--- src/components/MovieCardsContainer.js
+++ src/components/MovieCardsContainer.js
@@ -3,36 +3,28 @@ import MovieCard from "./MovieCard";
 import { useGlobalContext } from "../context";
 import Loader from "../pages/Loader";
 import Error from "../pages/Error";
-// dev json
+
 const MovieCardsContainer = () => {
-  const {searchParams} =useGlobalContext();
-  const url='http://www.omdbapi.com/?apikey=d377fe02&s='+searchParams
-  const {isLoading,isError,apiData} = useFetch(url,searchParams);
-  if(isLoading){
-    return (
-      <>
-      <Loader/>
-      </>
-    )
+  const { searchParams } = useGlobalContext();
+  const url = "http://www.omdbapi.com/?apikey=d377fe02&s=" + searchParams;
+  const { isLoading, isError, apiData } = useFetch(url, searchParams);
+
+  if (isLoading) {
+    return <Loader />;
   }
-  if(isError){
-    return (
-      <>
-      <Error/>
-      </>
-    )
+  if (isError) {
+    return <Error />;
   }
 
-  const {Search} = apiData
+  const { Search } = apiData;
+
   return (
     <section className="movie__cards-container">
-      {
-        Search.map((movie,index)=>{
-          return <MovieCard key={index} movie={movie}/>
-        })
-      }
+      {Search.map((movie, index) => {
+        return <MovieCard key={index} movie={movie} />;
+      })}
     </section>
-  )
-}
+  );
+};
 
-export default MovieCardsContainer
+export default MovieCardsContainer;
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,25 +1,29 @@
 import { useGlobalContext } from "../context";
 
 const SearchForm = () => {
-  const {setSearchValue,movieErrorRes} =useGlobalContext();
-  const inputHandler=(e)=>{
-    const value=e.currentTarget.value
+  const { setSearchValue, movieErrorRes } = useGlobalContext();
+  const inputHandler = (e) => {
+    const value = e.currentTarget.value;
 
-    if(value===''){
-      return setSearchValue('indian');
+    if (value === "") {
+      return setSearchValue("indian");
     }
-    setSearchValue(value)
-  }
+    setSearchValue(value);
+  };
 
   return (
     <section className="search__bar-container">
       <h1>Search Movies</h1>
-      <form className="form-control">
-        <input type="text"  className="form-control input1" onChange={inputHandler}/>
+      <form className="form-control" onSubmit={(e) => e.preventDefault()}>
+        <input
+          type="text"
+          className="form-control input1"
+          onChange={inputHandler}
+        />
       </form>
       <p className="result-info">{movieErrorRes}</p>
     </section>
-  )
-}
+  );
+};
 
 export default SearchForm;
